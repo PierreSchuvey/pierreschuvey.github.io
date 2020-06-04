91bba8869164766fbdc032b745a4e7aa0953d695
pierre <pierre.schuvey@l4m.fr>
2020-06-04 16:47:08 +0200

    test visio

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 document.querySelector('#start').addEventListener('click', function(e){
-  navigator.mediaDevices.getUserMedia({
-    video:true,
-    audio:true
-  }, function (stream){
-    let emitterVideo = document.querySelector('#emitter-video')
-    emitterVideo.src = windows.URL.createObjectURL(stream)
-    emitterVideo.play()
-  }, function(){})
+  navigator.mediaDevices.getUserMedia({  audio: true, video: true })
+         .then(function (stream) {
+           let emitterVideo = document.querySelector('#emitter-video')
+         emitterVideo.src = windows.URL.createObjectURL(stream)
+         emitterVideo.play()
+                       //Display the video stream in the video object
+          })
 })
