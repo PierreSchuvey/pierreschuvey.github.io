0c777b498023f9290073d5c6508a831285ab2e36
PierreSchuvey <32259099+PierreSchuvey@users.noreply.github.com>
2020-06-10 09:33:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app = require('express')();
     server = require('http').createServer(app);
     io = require('socket.io').listen(server);
     ent = require('ent'); // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
+
 // Chargement de la page index.html
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
